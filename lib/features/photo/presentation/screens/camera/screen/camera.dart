66537250e6dda4b9c5419660e413e7f834267581
Urban Potato <veryprogramming@gmail.com>
2026-03-03 01:51:50 +0700

    refactor: import

--- lib/features/photo/presentation/screens/camera/screen/camera.dart
+++ lib/features/photo/presentation/screens/camera/screen/camera.dart
@@ -2,12 +2,13 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../../shared/presentation/ui/index.dart'
+    show MessageWithButtonView;
 import '../../../provider/index.dart' show PhotoCubit;
 import '../provider/index.dart';
 import '../utils/camera_failure_message_helper.dart';
 import '../utils/permission_denied_message_helper.dart';
 import '../widgets/camera_view.dart';
-import '../../../../../../shared/presentation/ui/message_with_button_view/message_with_button_view.dart';
 
 class CameraScreen extends StatelessWidget {
   const CameraScreen({super.key});
